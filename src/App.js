0316fcca553c2fa87d61cc36a2c35c448d756220
alexandr <zbc@mail.ru>
2021-05-03 22:14:16 +0300

    12 урок

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
     return (
         <div className={'app-wrapper'}>
-            <header className={'header'}>
-                <img alt='logo' src={'https://svgx.ru/svg/1775543.svg'}/>
-            </header>
-            <nav className={'nav'}>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className={'content'}>
-                <div>
-                    <img src={'https://coverfiles.alphacoders.com/360/36044.jpg'} alt={'header'}/>
-                </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    my posts
-                    <div>
-                        new posts
-                    </div>
-                    <div>
-                        <div>
-                            post 1
-                        </div>
-                        <div>
-                            post 2
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <img alt='logo' src={'https://svgx.ru/svg/1775543.svg'}/>
+        </header>
+    );
+};
+
+export default Header;
