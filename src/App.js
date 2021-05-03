3d7e7239e9297bb16d27d57ccaf678cd8314fbc7
alexandr <zbc@mail.ru>
2021-05-03 20:04:33 +0300

    11 урок

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                <img alt='logo' src={'https://svgx.ru/svg/1775543.svg'}/>
+            </header>
+            <nav className={'nav'}>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className={'content'}>
+                <div>
+                    <img src={'https://coverfiles.alphacoders.com/360/36044.jpg'} alt={'header'}/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    my posts
+                    <div>
+                        new posts
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
