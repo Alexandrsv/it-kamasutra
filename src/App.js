83c0398e7291020c0f90a5e51145563e5777b392
alexandr <zbc@mail.ru>
2021-05-05 01:55:50 +0300

    20 урок

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Profile from "./components/Profile/Profile";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () => {
     return (
-        <div className={'app-wrapper'}>
-            <Header/>
-            <Navbar/>
-            {/*<div className={'app-wrapper-content'}>*/}
-            {/*    <Profile/>*/}
-            {/*</div>*/}
-            <div className={'app-wrapper-content'}>
-                <Dialogs/>
+        <BrowserRouter>
+            <div className={'app-wrapper'}>
+                <Header/>
+                <Navbar/>
+                <div className={'app-wrapper-content'}>
+                    <Route path={'/profile'} component={Profile}/>
+                    <Route path={'/dialogs'} component={Dialogs}/>
+                    {/*    <Profile/>*/}
+                    {/*<Dialogs/>*/}
+
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
