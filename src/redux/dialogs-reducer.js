a68d027fc9dd987068c470ef55d6ac0c3155db73
alexandr <zbc@mail.ru>
2021-05-12 01:26:49 +0300

    Урок 46 connect, mapStateToProps, mapDispatchToProps + немного спреда для редьюсера

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -18,13 +18,16 @@ let initialState = {
 export const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
-            state.newMessageBody = action.body
+            state = {...state, newMessageBody: action.body} 
             return state
         case SEND_MESSAGE:
             let body = state.newMessageBody
-            state.messages.push({id: Math.floor(Math.random() * 500), message: body})
-            state.newMessageBody = ''
-            return state
+            let nstate = {
+                ...state, 
+                messages: [...state.messages, {id: Math.floor(Math.random() * 500), message: body}],
+                newMessageBody: ''
+            }
+            return nstate
         default:
             return state
     }
