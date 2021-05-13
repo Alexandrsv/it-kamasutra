51b2b3942d36dc0a2e6cff230d11d90296d8448f
alexandr <zbc@mail.ru>
2021-05-13 22:10:19 +0300

    Урок 54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,12 +5,11 @@ import userPhoto from '../../assets/images/avatar-placeholder.png'
 
 
 class Users extends React.Component {
-    
-    constructor (props) {
-        super(props)
+
+    componentDidMount () {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then( response => {
-                props.setUsers(response.data.items)
+                this.props.setUsers(response.data.items)
             })
     }
 
