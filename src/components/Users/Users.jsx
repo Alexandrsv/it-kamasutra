0f0bd8ff3831558b3a15aa0134c4566df38f264e
alexandr <zbc@mail.ru>
2021-05-17 14:51:50 +0300

    72 урок

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import s from './Users.module.css'
 import userPhoto from '../../assets/images/avatar-placeholder.png'
 import { NavLink } from 'react-router-dom'
-import { usersAPI } from '../../api/api'
 
 
 let Users = (props) => {
